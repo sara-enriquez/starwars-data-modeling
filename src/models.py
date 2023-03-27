ac0d70ae01aa7596e558f3543cd5a1e8929a88d6
Sara Enríquez <sara_enriquezdesalamanca@hotmail.com>
2023-03-27 08:35:04 +0000

    created Star Wars DB diagram

--- src/models.py
+++ src/models.py
@@ -8,26 +8,62 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(120), nullable=False)
+    last_name = Column(String(120))
+    email = Column(String(250), nullable=False)
+    user_name = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    registration_date = Column (String(50))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__='character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(120), nullable=False)
+    description = Column(String(500), nullable=False)
+    birth_year = Column(String(50))
+    gender = Column(String(50))
+    height = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+
+class Planet(Base):
+    __tablename__='planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    description = Column(String(500), nullable=False)
+    climate = Column(String(120))
+    population = Column(Integer)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+
+class Vehicle(Base):
+    __tablename__='vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
+    description = Column(String(500), nullable=False)
+    passengers = Column(Integer)
+    model = Column(String(120))
+    crew = Column(String(120))
+    consumables = Column(String(120))
+    cost = Column(Integer)
+
+class Favs(Base):
+    __tablename__ = 'favs'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    vehicle = relationship(Vehicle)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
